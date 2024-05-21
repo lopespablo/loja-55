bd2a51b319b35df00d0ab3873aad6406fa6609e1
lopespablo <pablolopeskjk@gmail.com>
2024-05-20 23:20:20 -0300

    Initial commit

---
+++ pagina-do-produto/finalizar.js
@@ -0,0 +1,91 @@
+const products = [
+    { id: 1, ref:1234, name: "Camiseta", price: 49.99, image: "https://thugnine.fbitsstatic.net/img/p/camiseta-the-bite-70920/261749-2.jpg?w=800&h=800&v=202405021042&qs=ignoreimages/camiseta.jpg", description: "Uma linda camiseta.", otherImages: ["https://thugnine.fbitsstatic.net/img/p/camiseta-the-bite-70920/261751-3.jpg?w=605&h=605&v=202405021042&qs=ignore", "https://thugnine.fbitsstatic.net/img/p/camiseta-the-bite-70920/261751-4.jpg?w=605&h=605&v=202405021042&qs=ignore", "https://thugnine.fbitsstatic.net/img/p/camiseta-the-bite-70920/261751-5.jpg?w=605&h=605&v=202405021042&qs=ignore"] },
+
+    { id: 2, ref:1234, name: "Calça Jeans", price: 89.99, image: "https://thugnine.fbitsstatic.net/img/p/camiseta-the-bite-70920/261749-2.jpg?w=800&h=800&v=202405021042&qs=ignore", description: "Calça jeans de alta qualidade.", otherImages: ["https://thugnine.fbitsstatic.net/img/p/camiseta-the-bite-70920/261751-3.jpg?w=605&h=605&v=202405021042&qs=ignore", "https://thugnine.fbitsstatic.net/img/p/camiseta-the-bite-70920/261751-4.jpg?w=605&h=605&v=202405021042&qs=ignore", "https://thugnine.fbitsstatic.net/img/p/camiseta-the-bite-70920/261751-5.jpg?w=605&h=605&v=202405021042&qs=ignore"] },
+
+    { id: 3, ref:1234, name: "Tênis", price: 129.99, image: "https://thugnine.fbitsstatic.net/img/p/camiseta-the-bite-70920/261749-2.jpg?w=800&h=800&v=202405021042&qs=ignore", description: "Tênis confortável para o dia a dia.", otherImages: ["https://thugnine.fbitsstatic.net/img/p/camiseta-the-bite-70920/261751-3.jpg?w=605&h=605&v=202405021042&qs=ignore", "https://thugnine.fbitsstatic.net/img/p/camiseta-the-bite-70920/261751-4.jpg?w=605&h=605&v=202405021042&qs=ignore", "https://thugnine.fbitsstatic.net/img/p/camiseta-the-bite-70920/261751-5.jpg?w=605&h=605&v=202405021042&qs=ignore"] },
+];
+
+function getProductById(id) {
+    return products.find(product => product.id === parseInt(id));
+}
+
+function displayProductDetails() {
+    const params = new URLSearchParams(window.location.search);
+    const productId = params.get('id');
+    const product = getProductById(productId);
+
+    if (product) {
+        document.getElementById('nome-produto').textContent = product.name;
+        document.getElementById('ref-produto').textContent = `ref: ${product.ref}`;
+        document.getElementById('valor-produto').textContent = `R$ ${product.price.toFixed(2)}`;
+
+        const productDetail = document.getElementById('ft-principal');
+        productDetail.innerHTML = `
+            <img src="${product.image}" alt="${product.name}">
+        `;
+
+        const outrasimagens = document.getElementById('outras-imagens');
+        outrasimagens.innerHTML = ''; // Limpa quaisquer imagens existentes
+        product.otherImages.forEach((imgSrc, index) => {
+            const listItem = document.createElement('li');
+            listItem.id = `ft${index + 1}`;
+            listItem.className = 'fts';
+            listItem.innerHTML = `<img src="${imgSrc}" alt="${product.name} foto ${index + 1}">`;
+            outrasimagens.appendChild(listItem);
+        });
+    } else {
+        document.getElementById('product-detail').innerHTML = '<p>Produto não encontrado.</p>';
+    }
+}
+
+window.onload = displayProductDetails;
+
+
+document.querySelectorAll('.tms').forEach(function(div) {
+    div.addEventListener('click', function() {
+        this.classList.toggle('selecionado');
+    });
+});
+
+function enviarmsg() {
+    var camisasSelecionadas = [];
+    var informacoes = document.querySelector('.informacoes');
+    var camisa = informacoes.querySelector('h1').textContent;
+    var referencia = informacoes.querySelector('h2').textContent;
+    var tamanhosSelecionados = document.querySelectorAll('.tms.selecionado p');
+
+    tamanhosSelecionados.forEach(function(tam) {
+        var tamanho = tam.textContent;
+        camisasSelecionadas.push(camisa + ' (Ref: ' + referencia + ') ' + 'Tamanho: ' + tamanho);
+    });
+
+    var mensagem = "Olá! Estou interessado nas seguintes roupas: " + camisasSelecionadas.join(", ");
+    var numeroWhatsApp = "24999161001";
+    var linkWhatsApp = "https://wa.me/" + numeroWhatsApp + "?text=" + encodeURIComponent(mensagem);
+
+    window.open(linkWhatsApp);
+}
+
+
+
+    // SELECIONAR TAMANHO
+var tms = document.querySelectorAll('.tms');
+
+tms.forEach(sele => {
+    sele.addEventListener('click', function() {
+        if (this.style.backgroundColor === 'black') {
+            // Retorna ao estado original
+            this.style.backgroundColor = 'white';
+            this.style.color = 'black';
+        } else {
+            // Altera para o estado clicado
+            this.style.backgroundColor = 'black';
+            this.style.color = 'white';
+        }
+    });
+});
+    // SELECIONAR TAMANHO
+
+
+
---
+++ produtos.js
@@ -0,0 +1,79 @@
+
+    // BARRA DE PESQUISA
+function digitar() {
+    var NomeDigitado = document.getElementById('brpesquisa').value.toLowerCase();
+    var produtos = document.querySelectorAll('.produtos li');
+    
+    produtos.forEach((el) => {
+        var NomesProdutos = el.querySelector('h1').textContent.trim().toLowerCase();
+        if (NomeDigitado === '' || NomesProdutos.includes(NomeDigitado)) {
+            el.style.display = 'block'; // Mostra os itens que correspondem à pesquisa ou todos se a pesquisa estiver vazia
+        } else {
+            el.style.display = 'none'; // Oculta os itens que não correspondem à pesquisa
+        }
+    });
+}
+    // BARRA DE PESQUISA
+
+
+    // BARRA DE PEQUISA ITEMS
+document.addEventListener('DOMContentLoaded', function () {
+    var nomesItems = document.querySelectorAll('.nomesitems li');
+
+    nomesItems.forEach(function (item) {
+        item.addEventListener('click', function () {
+            var nomeItem = item.querySelector('p').textContent.toLowerCase();
+            document.getElementById('brpesquisa').value = nomeItem;
+            digitar(); // Chama a função de pesquisa para filtrar os produtos com base no novo valor da barra de pesquisa
+        });
+    });
+});
+    // BARRA DE PEQUISA ITEMS
+
+
+
+const products = [
+    {
+        id:1,
+        name: "Camiseta",
+        price: 49.99,
+        valor: '4x de 19,99',
+        image: "https://thugnine.fbitsstatic.net/img/p/camiseta-the-bite-70920/261749-2.jpg?w=800&h=800&v=202405021042&qs=ignore"
+    },
+    {
+        id:2,
+        name: "Calça Jeans",
+        price: 89.99,
+        valor: '4x de 19,99',
+        image: "https://thugnine.fbitsstatic.net/img/p/camiseta-the-bite-70920/261749-2.jpg?w=800&h=800&v=202405021042&qs=ignore"
+    },
+    {
+        id:3,
+        name: "Tênis",
+        price: 129.99,
+        valor: '4x de 19,99',
+        image: "https://thugnine.fbitsstatic.net/img/p/camiseta-the-bite-70920/261749-2.jpg?w=800&h=800&v=202405021042&qs=ignore"
+    }
+    // Adicione mais produtos aqui
+];
+
+function displayProducts() {
+    const productList = document.getElementById('lista-produtos');
+    products.forEach(product => {
+        const productDiv = document.createElement('li');
+        productDiv.className = '.produtos';
+        
+        productDiv.innerHTML = `
+        <a href="pagina-do-produto/paginafinal.html?id=${product.id}" target="_blank">
+                <img src="${product.image}" alt="${product.name}">
+                <h1 id="nome-p">${product.name}</h1>
+                <p id="valor">R$ ${product.price}</p>
+                <p id="valo-p">${product.valor}</p>
+        </a>
+        `;
+        
+        productList.appendChild(productDiv);
+    });
+}
+
+window.onload = displayProducts;
